8631d7c948080744c045962c43c3c6a04852b943
Dylan Wreggelsworth <dylan@bvr.io>
2017-03-08 22:01:30 +0100

    Added window types, WIP additional types

--- Sources/Alkali.h
+++ Sources/Alkali.h
@@ -6,13 +6,11 @@
 //  Copyright © 2017 BVR, LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 //! Project version number for Alkali.
-FOUNDATION_EXPORT double AlkaliVersionNumber;
+extern double AlkaliVersionNumber;
 
 //! Project version string for Alkali.
-FOUNDATION_EXPORT const unsigned char AlkaliVersionString[];
+extern const unsigned char AlkaliVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Alkali/PublicHeader.h>
 
---
+++ Sources/Shared/Window.swift
@@ -0,0 +1,72 @@
+//
+//  DSP.swift
+//  Alkali
+//
+//  Created by Dylan Wreggelsworth on 3/8/17.
+//  Copyright © 2017 BVR, LLC. All rights reserved.
+//
+
+import Foundation
+import Accelerate
+
+
+public enum Precision {
+    case float
+    case double
+}
+
+public enum Window<T: ExpressibleByFloatLiteral> {
+    public typealias Length = vDSP_Length
+
+    /// Represents a Hanning window
+    case hanning
+
+    /// Represents a Hanning window
+    case hamming
+
+    case blackman
+
+}
+
+extension Window where T == Double {
+
+    var function: (UnsafeMutablePointer<T>, vDSP_Length, Int32) -> Void {
+        switch self {
+        case .hamming:
+            return vDSP_hamm_windowD
+        case .hanning:
+            return vDSP_hann_windowD
+        case .blackman:
+            return vDSP_blkman_windowD
+        }
+    }
+
+
+    /// Returns a ```[Double]``` of the given length from this ```Window``` type.
+    public func buffer(of length: Length) -> [Double] {
+        var result = [Double](repeating: 0, count: Int(length))
+        function(&result, length, 0)
+        return result
+    }
+}
+
+extension Window where T == Float {
+
+    var function: (UnsafeMutablePointer<T>, vDSP_Length, Int32) -> Void {
+        switch self {
+        case .hamming:
+            return vDSP_hamm_window
+        case .hanning:
+            return vDSP_hann_window
+        case .blackman:
+            return vDSP_blkman_window
+        }
+    }
+
+    /// Returns a ```[Float]``` of the given length from this ```Window``` type.
+    public func buffer(of length: Length) -> [Float] {
+        var result = [Float](repeating: 0, count: Int(length))
+        function(&result, length, 0)
+        return result
+    }
+}
