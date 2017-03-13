1f4cff4bf4562ee90d9fbbe40be3d928c029bd2f
Dylan Wreggelsworth <dylan@bvr.io>
2017-03-13 16:42:18 +0100

    Use ValueArrays for later transforms, added more noise to test signal

--- Playgrounds/Alkali macOS.playground/Pages/General.xcplaygroundpage/Contents.swift
+++ Playgrounds/Alkali macOS.playground/Pages/General.xcplaygroundpage/Contents.swift
@@ -29,7 +29,7 @@ class FFTBuffer {
 class FFT {
     typealias Length = vDSP_Length
 
-    var n: Int     = 0
+    var n: Int = 0
     var halfN: Int = 0
     var log2N: Float  = 0
 
@@ -45,13 +45,26 @@ class FFT {
         resize(to: size)
     }
 
+    func upperPOT(_ value: Int) -> Int {
+        var v = value
+        v -= 1
+        v |= v >> 1
+        v |= v >> 2
+        v |= v >> 4
+        v |= v >> 8
+        v |= v >> 16
+        v += 1
+        return v
+    }
+
     func resize(to length: Int) {
         if let prexistingSetup = setup {
             vDSP_destroy_fftsetup(prexistingSetup)
         }
-        self.n = length
-        self.halfN = self.n / 2
-        self.log2N = round(log2f(Float(n)))
+
+        self.n = upperPOT(length)
+        self.log2N = roundf(log2f(Float(n)))
+        self.halfN = Int(log2N / 2.0)
 
         guard let fftSetup = vDSP_create_fftsetup(Length(log2N), FFTRadix(kFFTRadix2)) else {
             fatalError("Failed to init FFT!")
@@ -59,72 +72,72 @@ class FFT {
         setup = fftSetup
     }
 
-    func transform(buffer: FloatBuffer) -> ([Float], [Float]) {
+    func transform(buffer: FloatBuffer) -> (FloatBuffer, FloatBuffer) {
         guard let fftSetup = setup else {
             fatalError("FFT not setup!")
         }
-        var real = buffer.copy()
-        var imaginary = FloatBuffer(zeros: buffer.count)
-//        let magnitudes = FloatBuffer(zeros: buffer.count)
-//        let nMag = FloatBuffer(zeros: buffer.count)
-        let log2n = Length(roundf(log2f(Float(buffer.count))))
 
-        let bufferSizePOT = Int(1 << log2n)
-        var realp = [Float](repeating: 0, count: bufferSizePOT/2)
-        var imagp = [Float](repeating: 0, count: bufferSizePOT/2)
-        realp.count
-        var output = DSPSplitComplex(realp: &realp, imagp: &imagp)
-        let s = vDSP_create_fftsetup(log2n, Int32(kFFTRadix2))
-        buffer.pointer.withMemoryRebound(to: DSPComplex.self, capacity: 1) { ptr in
-            vDSP_ctoz(ptr, 2, &output, 1, Length(bufferSizePOT / 2))
-            vDSP_fft_zrip(s!, &output, 1, Length(log2n), FFTDirection(FFT_FORWARD))
+        var real = FloatBuffer(zeros: n / 2)
+        var imaginary = FloatBuffer(zeros: n / 2)
+
+        withPointers(&real, &imaginary) { (realp, imagp) in
+            var output = DSPSplitComplex(realp: realp, imagp: imagp)
+            buffer.pointer.withMemoryRebound(to: DSPComplex.self, capacity: 1) { ptr in
+                vDSP_ctoz(ptr, 2, &output, 1, Length(n / 2))
+                vDSP_fft_zrip(fftSetup, &output, 1, Length(log2N), FFTDirection(FFT_FORWARD))
+            }
         }
-//        vDSP_ctoz(UnsafePointer<DSPComplex>(buffer.pointer), 2, &output, 1, UInt(bufferSizePOT/2))
-//        withPointers(&real, &imaginary) { (realp, imagp) in
-//            var complex = DSPSplitComplex(realp: realp, imagp: imagp)
-//
-//            complex.realp.withMemoryRebound(to: DSPComplex.self, capacity: 1) {
-//                ptr in
-//                vDSP_ctoz(ptr, 1, &complex, 1, Length(halfN))
-//            }
-//
-//            vDSP_fft_zrip(fftSetup, &complex, 1, Length(log2N), FFTDirection(FFT_FORWARD))
-//
-//            real /= 2
-//            imaginary /= 2
-//
-//            guard let winBuff: FloatBuffer = self.window?.buffer(Length(self.n)) else {
-//                return
-//            }
-//
-//            real *= winBuff
-//            imaginary *= winBuff
-//        }
-        return (realp, imagp)
+
+        guard let winBuff: FloatBuffer = self.window?.buffer(Length(self.n)) else {
+            return (real, imaginary)
+        }
+
+        real      *= winBuff
+        imaginary *= winBuff
+
+        return (real, imaginary)
     }
 }
 
 var bufferS: FloatBuffer = FloatBuffer(count: 1024, repeatedValue: 0)
+var bufferT: FloatBuffer = FloatBuffer(count: 1024, repeatedValue: 0)
+var bufferU: FloatBuffer = FloatBuffer(count: 1024, repeatedValue: 0)
+var bufferV: FloatBuffer = FloatBuffer(count: 1024, repeatedValue: 0)
 
 //bufferS[1] = 1
 
+// Sin waves
 for i in 0..<bufferS.count {
-    let phi = Float(500 / 44100.0)
-    bufferS[i] = sin(2.0 * Float(i) * phi * Float.pi)
+    let phi = Float(250 / 44100.0)
+    bufferS[i] = sinf(Float(i) * phi * Float.pi)
+}
+for i in 0..<bufferT.count {
+    let phi = Float(750 / 44100.0)
+    bufferT[i] = sinf(Float(i) * phi * Float.pi)
+}
+for i in 0..<bufferU.count {
+    let phi = Float(4000 / 44100.0)
+    bufferU[i] = sinf(Float(i) * phi * Float.pi)
 }
 
-//for i in 0..<bufferS.count {
-//    if i % 2 == 1 {
-//        bufferS[i] = -1.0
-//    }
-//}
+//Rect
+for i in 0..<bufferV.count {
+    let phi = Float(0 / 44100.0)
+    bufferV[i] = sinf(Float(i) * phi * Float.pi) > 0 ? -1 : 1
+}
+bufferV.map { $0 }
+
+bufferT.map { $0 }
+
 
 bufferS.map { $0 }
 
+let combined = (bufferT + bufferS + bufferU + bufferV)
+combined.map { $0 }
+
 
 let fft = FFT(size: bufferS.count)
-let result = fft.transform(buffer: bufferS)
-
+let result = fft.transform(buffer: combined)
 result.0.map { $0 }
 
 result.1.map { $0 }
