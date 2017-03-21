ecaf218bfbdcbe2262700b21992c6a009f2bf4b0
Dylan Wreggelsworth <dylan@bvr.io>
2017-03-21 13:52:15 +0100

    Messing with conversion apis

--- Playgrounds/Alkali macOS.playground/Pages/Math.xcplaygroundpage/Contents.swift
+++ Playgrounds/Alkali macOS.playground/Pages/Math.xcplaygroundpage/Contents.swift
@@ -4,6 +4,12 @@ import Foundation
 import Alkali
 import Upsurge
 
+let amp = 120.0
+let db = Math.dB(from: amp)
+
+db.amps()
+amp.dBs()
+
 
 
 //: [Next](@next)
--- Sources/Shared/Math.swift
+++ Sources/Shared/Math.swift
@@ -12,6 +12,8 @@ import Upsurge
 public enum Math {
     public static let silenceCutoff = 1e-9
     public static let silenceCutoffdB = -90
+    public static let log10over20: Double = log(10.0)/20.0
+    public static let log10over20f: Float = logf(10.0)/20.0
 
     public static func isPOT(_ value: Int) -> Bool {
         return (value == 1 || (value & (value - 1)) == 0)
@@ -35,5 +37,29 @@ public enum Math {
 
     public static func instantPower<T: LinearType>(_ x: T) -> Float where T.Element == Float {
         return energy(x) / Float(x.count)
+    }
+
+    public static func amp(from dB: Decibel) -> Amp {
+        if dB > -150.0 {
+            return exp(dB * log10over20)
+        } else {
+            return 0
+        }
+    }
+
+    public static func dB(from amp: Amp) -> Decibel {
+        return 20 * log10(amp)
+    }
+}
+
+extension Amp {
+    public func dBs() -> Decibel {
+        return Math.dB(from: self)
+    }
+}
+
+extension Decibel {
+    public func amps() -> Amp {
+        return Math.amp(from: self)
     }
 }
