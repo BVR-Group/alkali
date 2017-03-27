57ba0bcb9da2bcb15bb930ee2181cf3dd88d5514
Dylan Wreggelsworth <dylan@bvr.io>
2017-03-27 18:33:27 +0200

    Scale image

--- Playgrounds/Alkali iOS.playground/Pages/UI.xcplaygroundpage/Contents.swift
+++ Playgrounds/Alkali iOS.playground/Pages/UI.xcplaygroundpage/Contents.swift
@@ -4,10 +4,14 @@ import Foundation
 import Alkali
 import UIKit
 
-let size = (width: 72, height: 72)
+let size = (width: 16, height: 16)
 var pixelData = PixelBuffer.testPattern(size)
-let image = UIImageView(image: UIImage.from(pixelData))
-image
+let original = UIImage.from(pixelData)
+let image = UIImage.init(cgImage: original!.cgImage!, scale: original!.scale * 0.0625, orientation: original!.imageOrientation)
+
+
+UIImageView(image: image)
+
 //
 //dump(pixelData.data)
 
