a76c8d4fb3f45a0c581dc59c9276c514445c73f4
Dylan Wreggelsworth <dylan@bvr.io>
2017-03-30 23:44:48 +0200

    Working on spectrogram rendering

--- Playgrounds/Alkali iOS.playground/Pages/UI.xcplaygroundpage/Contents.swift
+++ Playgrounds/Alkali iOS.playground/Pages/UI.xcplaygroundpage/Contents.swift
@@ -3,27 +3,85 @@
 import Foundation
 import Alkali
 import UIKit
-let dimension = 16
+import Accelerate
+import AVFoundation
+import PlaygroundSupport
+import Upsurge
 
+let dimension = 32
 let size = CGSize(width: dimension, height: dimension)
-var pixelData = PixelCanvas.testPattern(size)
+let canvas = PixelCanvas(of: size)
 
-//pixelData.line(from: CGPoint(x: 3, y:3), to: CGPoint(x: 10,y: 10), color: .red)
-pixelData
-    .line(from: CGPoint(x: 0, y: 8), to: CGPoint(x: 8, y: 0), with: .black)
-    .line(from: CGPoint(x: 0, y: 0), to: CGPoint(x: 8, y: 8), with: .red)
-    .line(from: CGPoint(x: 4, y: 0), to: CGPoint(x: 4, y: 3), with: .blue)
+func loadFile() {
+//    let view: UIImageView
 
-pixelData[8, 8] = .green
+    guard let audioPath = Bundle.main.path(forResource: "DoomTest", ofType: "wav") else {
+        return
+    }
 
-let original = pixelData.image?.resized(to: CGSize(width: 256, height: 256))
+    let audioFile = URL(fileURLWithPath: audioPath)
+    let asset = AVURLAsset(url: audioFile, options: [AVURLAssetPreferPreciseDurationAndTimingKey: NSNumber(value: true as Bool)])
 
-let newImage = pixelData
-pixelData[9, 8] = .green
+    guard let assetTrack = asset.tracks(withMediaType: AVMediaTypeAudio).first else {
+        return
+    }
 
-UIImageView(image: newImage.image?.resized(to: CGSize(width: 256, height: 256)))
-UIImageView(image: original)
+    guard let reader = try? AVAssetReader(asset: asset) else {
+        return
+    }
 
+    let outputSettings: [String : Any] = [
+        AVFormatIDKey:                  Int(kAudioFormatLinearPCM),
+        AVLinearPCMBitDepthKey:         16,
+        AVLinearPCMIsBigEndianKey:      false,
+        AVLinearPCMIsFloatKey:          false,
+        AVLinearPCMIsNonInterleaved:    false
+    ]
+    let readerOutput = AVAssetReaderTrackOutput(track: assetTrack, outputSettings: outputSettings)
+    readerOutput.alwaysCopiesSampleData = false
+    reader.add(readerOutput)
+
+    reader.startReading()
+
+    var buffer = Data()
+
+    while reader.status == .reading {
+        guard let readSampleBuffer = readerOutput.copyNextSampleBuffer(),
+              let readBuffer = CMSampleBufferGetDataBuffer(readSampleBuffer) else {
+                break
+        }
+        var readBufferLength = 0
+        var readBufferPointer: UnsafeMutablePointer<Int8>?
+        CMBlockBufferGetDataPointer(readBuffer, 0, &readBufferLength, nil, &readBufferPointer)
+        buffer.append(UnsafeBufferPointer(start: readBufferPointer, count: readBufferLength))
+        CMSampleBufferInvalidate(readSampleBuffer)
+        let samplesToProcess = buffer.count / MemoryLayout<Int16>.size
+        if samplesToProcess > 0 {
+            drawNumberOfSamples(n: samplesToProcess, to: canvas, from: &buffer)
+        }
+
+    }
+}
+
+func drawNumberOfSamples(n: Int, to canvas: PixelCanvas, from data: inout Data) {
+    data.withUnsafeBytes { (ptr: UnsafePointer<Int16>) in
+        var buffer = FloatBuffer(count: n)
+        buffer.withUnsafeMutablePointer { bufferPtr in
+            let sampleCount = vDSP_Length(n)
+            vDSP_vflt16(ptr, 1, bufferPtr, 1, sampleCount)
+        }
+    }
+
+    data.removeFirst(n * MemoryLayout<Int16>.size)
+}
+
+loadFile()
+
+
+//PlaygroundPage.current.liveView = view
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//audioPlayer?.play()
 
 //
 //dump(pixelData.data)
